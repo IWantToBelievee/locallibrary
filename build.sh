55d62891d4dcc8101032576f17d22be9396233cf
IWantToBelieve <artemkursaevkz@gmail.com>
2024-03-28 12:13:50 +0500

    COMMIT

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-create_env="python -m venv venv"
-activate='source venv/bin/activate'
-install='pip install requirements.txt'
+command "python -m venv venv"
+command 'source venv/bin/activate'
+command 'pip install -r requirements.txt'
